36daa32496809beb2fea7ef71411adb8f5d5cf12
feodor <lebedevf@list.ru>
2023-03-04 12:49:21 +0000

    fixed bad arg

--- edit_paste.php
+++ edit_paste.php
@@ -16,31 +16,32 @@ $result = $stmt->fetch();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bitbin</title>
-    <link rel="stylesheet" href="css/paste.css">
-    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+    <title>Pastebin</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 
-<body>
-    <h1 class="logo">
-Pastebin
-    </h1>
+<body class="bg-dark text-white">
+<div class="container">
+    <h1 class="logo">Pastebin</h1>
 
     <form action="update.php?id=<?php echo $id; ?>" method="POST">
-        <textarea name="update_content" id="" cols="30" rows="30"><?php echo $result["content"]; ?></textarea>
+<textarea name="content" id="" cols="30" rows="20" class="w-100 p-3"><?php echo $result["content"]; ?></textarea>
+</br>
+
 
         <div class="buttons-container">
             <div class="button">
-                <a href="update.php?id=<?php echo $id; ?>"><button>Save BitBin</button></a>
+                <a href="update.php?id=<?php echo $id; ?>"><button class="btn btn-success">Save Paste</button></a>
             </div>
         </div>
     </form>
-
+</br>
     <div class="buttons-container">
         <div class="button">
-            <a href="paste.php?id=<?php echo $id; ?>"><button>Back to the paste</button></a>
+            <a href="paste.php?id=<?php echo $id; ?>"><button class="btn btn-danger">Back to the Paste</button></a>
         </div>
     </div>
+</div>
 </body>
 
 </html>
